830568010b62d3af873e30ee0ca627186e396e21
Лебедев Дмитрий Сергеевич <ds.leb@kontur>
2019-10-24 18:56:16 +0500

    feat: change telegram channel name

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
-export const channelId = '@reatex_channel';
+export const channelId = '@random2ch_channel';
 export const baseUrl = 'https://2ch.hk';
 export const url = 'https://2ch.hk/b/catalog_num.json';
 export const corsProxyHost = '0.0.0.0';
